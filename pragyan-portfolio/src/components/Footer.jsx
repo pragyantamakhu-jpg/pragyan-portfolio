cbd2b2b3de32161c0d490e824821f169bd212aca
Pragyan Tamakhu <pragyantamakhu@gmail.com>
2026-06-19 12:49:19 +0545

    Update GitHub and LinkedIn links

--- pragyan-portfolio/src/components/Footer.jsx
+++ pragyan-portfolio/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="pt-8 border-t border-[#2a2a3a] flex justify-between items-center flex-wrap gap-4">
-      <span className="text-[11px] text-muted">© 2026 · Pragyan Tamakhu · Bhaktapur, Nepal</span>
+      <span className="text-[11px] text-muted">
+        © 2026 · Pragyan Tamakhu · Bhaktapur, Nepal
+      </span>
       <div className="flex gap-5">
         <a
           href="mailto:pragyantamakhu@gmail.com"
@@ -10,7 +12,7 @@ export default function Footer() {
           email
         </a>
         <a
-          href="https://github.com/"
+          href="https://github.com/pragyantamakhu-jpg"
           target="_blank"
           rel="noreferrer"
           className="text-[11px] tracking-widest uppercase text-muted hover:text-accent2 transition-colors duration-200 no-underline"
@@ -18,7 +20,7 @@ export default function Footer() {
           github
         </a>
         <a
-          href="https://linkedin.com/"
+          href="www.linkedin.com/in/pragyan-tamakhu-6b9416285"
           target="_blank"
           rel="noreferrer"
           className="text-[11px] tracking-widest uppercase text-muted hover:text-accent2 transition-colors duration-200 no-underline"
@@ -27,5 +29,5 @@ export default function Footer() {
         </a>
       </div>
     </footer>
-  )
+  );
 }
